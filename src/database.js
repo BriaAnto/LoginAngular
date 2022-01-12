a495d7261b380f95f3f311427ff36b1d16af6e3c
BriaAnto <97619566+BriaAnto@users.noreply.github.com>
2022-01-12 13:56:36 -0600

    Delete database.js

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/login', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-
-})
-    .then(db => console.log('Database is connected'))
-    .catch(err => console.log(err));
